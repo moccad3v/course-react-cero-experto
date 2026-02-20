ae1563c30110e244b5a07bcd173c044fab9662aa
moccad3v <jdavidpascuas@gmail.com>
2026-02-19 23:15:40 -0500

    First commit, Recap of variable creation and template strings

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+
+
+const firstName: string = 'Juanito';
+const lastName = 'Pascuas';
+
+let diceNumber = 5;
+diceNumber = 2;
+
+const constainsLetterA = lastName.includes('a');
+
+console.log({ constainsLetterA, diceNumber, firstName, lastName })
---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,6 @@
+const firstName = 'Juanchis';
+const lastName = "Pascuas O'Neal";
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
