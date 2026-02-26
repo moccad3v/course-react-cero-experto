7199a039eedc53b5bc46780072e0a09860632a65
Juan Pascuas (Moccad3v) <jdavidpascuas@gmail.com>
2026-02-25 22:55:08 -0500

    Recap, Type correctly before creating arrays or other constants.

--- 01-reforzamiento/src/bases/04-arrays.ts
+++ 01-reforzamiento/src/bases/04-arrays.ts
@@ -1,9 +1,9 @@
-const myArray: (number | string)[] = [1, 2, 3, 4, 5, 6];
+const myArray: number[] = [1, 2, 3, 4, 5, 6];
 
-const myArray2 = structuredClone(myArray);
+const myArray2 = [...myArray];
 
-myArray.push('Hola Mundo');
-// myArray.push(8);
+// myArray.push('Hola Mundo');
+myArray.push(8);
 
 console.log({myArray, myArray2});
 
