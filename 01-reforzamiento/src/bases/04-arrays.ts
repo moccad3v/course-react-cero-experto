ee7dfbdf15de25a00edc3a212b152cc82d54dd7b
Juan Pascuas (Moccad3v) <jdavidpascuas@gmail.com>
2026-02-25 22:43:23 -0500

    Recap, use arrays

---
+++ 01-reforzamiento/src/bases/04-arrays.ts
@@ -0,0 +1,10 @@
+const myArray: number[] = [1, 2, 3, 4, 5, 6];
+
+// myArray.push(7);
+// myArray.push(8);
+
+console.log(myArray);
+
+// for (const myNumber of myArray) {
+//   console.log(myNumber + 10);
+// }
