d6490df9faf55f430e1cb38c97091933dcb7ee88
Juan Pascuas (Moccad3v) <jdavidpascuas@gmail.com>
2026-03-16 20:45:08 -0500

    Recap, create arrow function

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,6 +2,12 @@ function greet (name: string): string {
     return `Hi ${name}`;
 }
 
+const greet2 = (name: string): string => {
+    return `Hi ${name}`;
+}
+
 const message = greet('Mocca');
+const message2 = greet2('Juan');
 
 console.log(message);
+console.log(message2);
