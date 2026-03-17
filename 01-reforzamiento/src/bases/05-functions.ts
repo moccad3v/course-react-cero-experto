9b8d3326302eb6fcba8434a7d8a5ba6107520602
Juan Pascuas (Moccad3v) <jdavidpascuas@gmail.com>
2026-03-16 20:53:42 -0500

    Recap, add getUserData functions

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -11,3 +11,22 @@ const message2 = greet2('Juan');
 
 console.log(message);
 console.log(message2);
+
+function getUserData () {
+    return {
+        name: 'Mocca',
+        email: 'moccad3v@gmail.com'
+    };
+}
+
+const getUserData2 = () => {
+    return {
+        name: 'Mocca',
+        email: 'moccad3v@gmail.com'
+    }
+}
+
+const user = getUserData();
+const user2 = getUserData2();
+
+console.log(user, user2);
